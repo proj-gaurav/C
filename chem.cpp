546a39786c8a7c1f298d9b673577db1567b8ac13
proj-gaurav <94140850+proj-gaurav@users.noreply.github.com>
2021-11-12 01:40:39 +0530

    Add files via upload
    Making this for calculating numerical in Chemistry and Physics.

---
+++ chem.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>     //for power(^) exponents
+#include <iomanip>    //setprecision for digits after decimal
+using namespace std;
+
+double sum(double a, double b){
+    return (a+b);
+}
+double sub(double a, double b){
+    return (a-b);
+}
+double multiply(double a, double b){
+    return (a*b);
+}
+double divide(double a, double b){
+    return (a/b);
+}
+
+int main()
+{
+    double a,b;
+    cin>>a>>b;
+    cout<<fixed;  //Works only for Digits after decimal
+    cout<<setprecision(4)<<sub(a,b);
+    return 0;
+}
