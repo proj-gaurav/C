ca5ff125ec0cb15a00c68d50943cc5e56f23147e
Gaurav Gadiya <94140850+proj-gaurav@users.noreply.github.com>
2021-11-12 13:47:47 +0530

    Update chem.cpp

--- chem.cpp
+++ chem.cpp
@@ -20,7 +20,7 @@ int main()
 {
     double a,b;
     cin>>a>>b;
-    cout<<fixed;  //It will fix numbers before Dicimal point
+    cout<<fixed;  //It will fix numbers before Decimal point
     cout<<setprecision(4)<<sub(a,b);
     return 0;
 }
