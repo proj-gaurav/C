a9b0a7e67fc9b51ff24bfd8e1bc3a7f13dab1c24
Gaurav Gadiya <94140850+proj-gaurav@users.noreply.github.com>
2021-11-12 13:34:48 +0530

    Add files via upload
    Stage 1 : Using If-Else for function-overloading

---
+++ alpha-stage.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>   //for power(^) exponents
+#include <iomanip> //setprecision for digits after decimal
+using namespace std;
+
+double sum(double a, double b)
+{
+    return (a + b);
+}
+double sub(double a, double b)
+{
+    return (a - b);
+}
+double multiply(double a, double b)
+{
+    return (a * b);
+}
+double divide(double a, double b)
+{
+    return (a / b);
+}
+// double modulo(double a, double b)
+// {
+//     return (a % b);
+// }
+
+int main()
+{
+    double a, b, result;
+    char opr;
+    cout << "Enter First Value : ";
+    cin >> a;
+    cout << "Enter Second Value : ";
+    cin >> b;
+    cout << fixed; //Works only for Digits after decimal
+    cout << setprecision(4);
+    cout << "Enter Your Operator : ";
+    cin >> opr;
+    
+    if (opr=='+')
+    {
+        result = sum(a, b);
+        cout<<"Your Answer : "<<result<<endl;
+    }
+    else if (opr=='-')
+    {
+        result = sub(a, b);
+        cout<<"Your Answer : "<<result<<endl;
+    }
+    else if (opr=='*')
+    {
+        result = multiply(a, b);
+        cout<<"Your Answer : "<<result<<endl;
+    }
+    else if (opr=='/')
+    {
+        result = divide(a, b);
+        cout<<"Your Answer : "<<result<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
